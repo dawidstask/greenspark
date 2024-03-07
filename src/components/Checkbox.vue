406fae4b6a4d41f0ed8af5c5ba67701082f3b7d8
Dawid Staskiewicz <dawstask@gmail.com>
2024-03-07 20:39:12 +0100

    wip: widgets, astyle widget, add checkbox and logo

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<label class="container">
+		<input type="checkbox">
+		<span class="checkmark"></span>
+	</label>
+</template>
+
+<style scoped>
+
+/* The container */
+.container {
+	display: block;
+	position: relative;
+	padding-left: 35px;
+	margin-bottom: 12px;
+	cursor: pointer;
+	font-size: 22px;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+	position: absolute;
+	opacity: 0;
+	cursor: pointer;
+	height: 0;
+	width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+	position: absolute;
+	top: 0;
+	right: 0;
+	height: 16px;
+	width: 16px;
+	border: 1px solid var(--black);
+	border-radius: 3px;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+	background-color: #AFC6BD;
+	border: 1px solid gray;
+
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+	background-color: var(--green);
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+	content: "";
+	position: absolute;
+	display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+	display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+	left: 5px;
+	top: 0;
+	width: 5px;
+	height: 10px;
+	border: solid white;
+	border-width: 0 3px 3px 0;
+	-webkit-transform: rotate(45deg);
+	-ms-transform: rotate(45deg);
+	transform: rotate(45deg);
+}
+</style>
--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -2,6 +2,7 @@
 import {Product} from "../types/Product.ts";
 import {Type} from "../types/Type.ts";
 import Tooltip from "./Tooltip.vue";
+import Checkbox from "./Checkbox.vue";
 
 const props = defineProps<{
 	product: Product
@@ -15,25 +16,28 @@ const getValue = () => {
 
 <template>
 	<div class="widget">
-		<pre>{{ product }}</pre>
-		<div class="widget__header">
-			<span>This product {{ product.action }}</span>
-			<span>{{ getValue() }}</span>
+		<div class="widget__header flex align-items-center" :class="`widget__header--${product.action}`">
+			<img :src="product.type === Type.CARBON ? '/logo_green.svg' : '/logo_white.svg'" class="widget__logo" alt="logo" />
+			<div class="flex flex-column">
+				<span class="widget__info">This product {{ product.action }}</span>
+				<span class="widget__value">{{ getValue() }}</span>
+			</div>
 		</div>
 		<div class="widget__content">
-			<p class="flex justify-content-between">
-				<span class="flex">Link to Public Profile<Tooltip class="align-content-start" /></span><input type="checkbox" id="profile" name="profile" value="selected">
-			</p>
-			<p>
-				<span>Badge colour</span>
-			</p>
-			<p>
-				<span>Activate badge</span>
+			<div class="flex justify-content-between">
+				<span class="widget__row flex">Link to Public Profile <Tooltip class="align-content-start" /></span>
+				<Checkbox />
+			</div>
+			<div>
+				<span class="widget__row">Badge colour</span>
+			</div>
+			<div class="flex justify-content-between">
+				<span class="widget__row">Activate badge</span>
 				<label class="switch">
 					<input type="checkbox">
 					<span class="slider round"></span>
 				</label>
-			</p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -42,5 +46,41 @@ const getValue = () => {
 .widget {
 	width: 221.32px;
 	height: auto;
+}
+.widget__header {
+	color: white;
+	height: 66px;
+	border-radius: 5.95px;
+}
+.widget__logo {
+	width: 39.7px;
+	height: 45.41px;
+	margin: 0 10px
+}
+.widget__info {
+	font-weight: 400;
+	font-size: 12.41px;
+	line-height: 15.07px;
+}
+.widget__value {
+	font-weight: 700;
+	font-size: 17.86px;
+	line-height: 21.41px;
+}
+.widget__row {
+	color: var(--green);
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 17.01px;
+}
+.widget__header--collects {
+	background-color: var(--blue);
+}
+.widget__header--plants {
+	background-color: var(--green);
+}
+.widget__header--offsets {
+	background-color: var(--beige);
+	color: var(--green);
 }
 </style>
