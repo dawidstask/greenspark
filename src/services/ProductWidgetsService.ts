5a25aa3b3f2b00782a02e01f8f43b1eb2c316185
Dawid Staskiewicz <dawstask@gmail.com>
2024-03-10 20:06:19 +0100

    add readme

--- src/services/ProductWidgetsService.ts
+++ src/services/ProductWidgetsService.ts
@@ -3,7 +3,7 @@ import api from "../utils/api.ts";
 import {AxiosResponse} from "axios";
 
 class ProductWidgetsService {
-	getWidgets() {
+	getWidgets(): Promise<Product[]> {
 		return api.get<Product[]>('/product-widgets')
 			.then((response: AxiosResponse<Product[]>) => response.data)
 	}
